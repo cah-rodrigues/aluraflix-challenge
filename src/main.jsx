dd3f73a4d74671d5eed314efaa0791b2ae1dc6e5
Carla Rodrigues <rodricah1.8@gmail.com>
2024-07-16 18:50:58 -0400

    fix: deploy routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { Home, NewVideo } from './pages'
 import { theme, GlobalStyle } from './styles'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/new-video',
-    element: <NewVideo />
-  }  
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <BrowserRouter basename='/aluraflix-challenge'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new-video' element={<NewVideo />} /> 
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
     <GlobalStyle />
   </React.StrictMode>,
